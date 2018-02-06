7835cc88eec458d79584bd06f5e514271f535676
Ken Reese <krrgithub@gmail.com>
2018-02-05 20:40:02 -0700

    Curtis told me to push

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import renderer.svgrenderer
 from core.room import RoomFactory, Room
 from core.floorplan import FloorPlan
+from core.edge import Orientation
 
 def main():
+
+    room = RoomFactory.Rectangle(10, 12)
+
+    roomA, roomB = room.subdivide(4, 4, Orientation.Horizontal)
+    roomB, roomC = roomB.subdivide(2, 6, Orientation.Vertical)
+    roomA, roomD = roomA.subdivide(2, 2, Orientation.Vertical)
+
     fp = FloorPlan([
-        RoomFactory.Rectangle(10, 12)
+        roomA, roomB, roomC, roomD
     ])
 
     renderer.svgrenderer.SvgRenderer.render_plan(fp)
